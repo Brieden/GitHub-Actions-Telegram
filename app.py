7b36d86281f642ba31f5236729df76f7a6c1bc0f
Sven Brieden <sven.brieden@gmail.com>
2021-11-29 16:03:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import sys
 import requests
 
-telegram_token = sys.argv[0]
-telegram_chat_id = sys.argv[1]
+telegram_token = sys.argv[1]
+telegram_chat_id = sys.argv[2]
 
-response = requests.post(
+telegram_response = requests.get(
         url=f'https://api.telegram.org/bot{telegram_token}/sendMessage',
-        data={'chat_id': telegram_chat_id, 'text': 'Hello world'}
+        data={'chat_id': str(telegram_chat_id), 'text': 'Hello world'}
     ).json()
 
-print(response)
+print(telegram_response)
